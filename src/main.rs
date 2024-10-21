bbcf77f628bd800d1f443ebb32db53d69361fcc3
arkhipov.iv99@mail.ru <arkhipov.iv99@mail.ru>
2024-10-21 18:06:50 +0300

    Example with lvp

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use esp_hal::entry;
 use esp_println::println;
 use esp_wifi::{
     current_millis,
-    esp_now::{EspNow, PeerInfo, BROADCAST_ADDRESS},
+    esp_now::{EspNow, BROADCAST_ADDRESS},
 };
 use martos::get_esp_now;
 use martos::init_system;
@@ -16,6 +16,8 @@ use martos::task_manager::TaskManager;
 static mut ESP_NOW: Option<EspNow> = None;
 /// Variable for saving time to send broadcast message
 static mut NEXT_SEND_TIME: Option<u64> = None;
+/// Value for local vote protocol.
+static mut VALUE: u8 = 1;
 
 /// Setup function for task to execute.
 fn setup_fn() {
@@ -30,40 +32,31 @@ fn setup_fn() {
 fn loop_fn() {
     unsafe {
         let mut esp_now = ESP_NOW.take().expect("Esp-now error in main");
+        let mut value = VALUE;
 
+        // Receive value from neighbours
         let r = esp_now.receive();
         if let Some(r) = r {
             println!("Received {:?}", r);
-
-            if r.info.dst_address == BROADCAST_ADDRESS {
-                if !esp_now.peer_exists(&r.info.src_address) {
-                    esp_now
-                        .add_peer(PeerInfo {
-                            peer_address: r.info.src_address,
-                            lmk: None,
-                            channel: None,
-                            encrypt: false,
-                        })
-                        .unwrap();
-                }
-                let status = esp_now
-                    .send(&r.info.src_address, b"Hello Peer")
-                    .unwrap()
-                    .wait();
-                println!("Send hello to peer status: {:?}", status);
-            }
+            let data = r.get_data();
+            let received_value = data[0];
+            // Local voting protocol
+            value = (value + received_value) / 2;
         }
 
+        // Send value to neighbours
         let mut next_send_time = NEXT_SEND_TIME.take().expect("Next send time error in main");
         if current_millis() >= next_send_time {
             next_send_time = current_millis() + 5 * 1000;
             println!("Send");
             let status = esp_now
-                .send(&BROADCAST_ADDRESS, b"0123456789")
+                .send(&BROADCAST_ADDRESS, &[value.try_into().unwrap()])
                 .unwrap()
                 .wait();
             println!("Send broadcast status: {:?}", status)
         }
+
+        VALUE = value;
 
         NEXT_SEND_TIME = Some(next_send_time);
         ESP_NOW = Some(esp_now);
