eebcd6a4d4a977e155012bae081291591fce0b8c
arkhipov.iv99@mail.ru <arkhipov.iv99@mail.ru>
2024-10-24 13:53:39 +0300

    Add example for esp32

--- esp32/README.md
+++ esp32/README.md
@@ -1,4 +1,4 @@
-# Rust example for risc-v esp32c6 architecture
+# Rust example for xtensa esp32 architecture
 
 Presented here is a straightforward Rust example utilizing Martos with wifi usage.
 
@@ -6,7 +6,7 @@ A broadcast signal is periodically sent, and is also informed about the receipt 
 
 ## How to install dependencies
 
-For comprehensive guidance on installing the necessary dependencies for developing applications targeting the RISC-V ESP32-C6 architecture,
+For comprehensive guidance on installing the necessary dependencies for developing applications targeting the Xtensa ESP32 architecture,
 please refer to [the official website](https://docs.esp-rs.org/book/installation/riscv-and-xtensa.html).
 Below is an illustrative example demonstrating the installation of building toolchains on a Linux (Ubuntu/Debian):
 ```
@@ -19,7 +19,7 @@ espup install
 
 ## How to build the example
 
-For a thorough guide on developing projects for the RISC-V ESP32-C6 architecture across various operating systems,
+For a thorough guide on developing projects for the Xtensa ESP32 architecture across various operating systems,
 we recommend consulting [the official website](https://docs.esp-rs.org/book/installation/riscv-and-xtensa.html#3-set-up-the-environment-variables).
 Below, you will find an illustrative example showcasing the building process on a Linux system (Ubuntu/Debian):
 ```
@@ -28,7 +28,7 @@ cargo build --release
 ```
 
 ## How to run the example
-For detailed instructions on running projects for the RISC-V ESP32-C6 architecture across various operating systems,
+For detailed instructions on running projects for the Xtensa ESP32 architecture across various operating systems,
 we recommend consulting [the official website](https://docs.esp-rs.org/book/tooling/espflash.html).
 Below, you will find an illustrative example showcasing the running on a Linux system (Ubuntu/Debian):
 ```
---
+++ esp32/src/main.rs
@@ -0,0 +1,79 @@
+#![no_std]
+#![no_main]
+
+use esp_backtrace as _;
+use esp_hal::entry;
+use esp_println::println;
+use esp_wifi::{
+    current_millis,
+    esp_now::{EspNow, BROADCAST_ADDRESS},
+};
+use martos::get_esp_now;
+use martos::init_system;
+use martos::task_manager::TaskManager;
+
+/// Esp-now object for network
+static mut ESP_NOW: Option<EspNow> = None;
+/// Variable for saving time to send broadcast message
+static mut NEXT_SEND_TIME: Option<u64> = None;
+/// Value for local vote protocol.
+static mut VALUE: u8 = 1;
+
+/// Setup function for task to execute.
+fn setup_fn() {
+    println!("Setup hello world!");
+    unsafe {
+        ESP_NOW = Some(get_esp_now());
+        NEXT_SEND_TIME = Some(current_millis() + 5 * 1000);
+    }
+}
+
+/// Loop function for task to execute.
+fn loop_fn() {
+    unsafe {
+        let mut esp_now = ESP_NOW.take().expect("Esp-now error in main");
+        let mut value = VALUE;
+
+        // Receive value from neighbours
+        let r = esp_now.receive();
+        if let Some(r) = r {
+            println!("Received {:?}", r);
+            let data = r.get_data();
+            let received_value = data[0];
+            // Local voting protocol
+            // value = (value + received_value) / 2;
+        }
+
+        // Send value to neighbours
+        let mut next_send_time = NEXT_SEND_TIME.take().expect("Next send time error in main");
+        if current_millis() >= next_send_time {
+            next_send_time = current_millis() + 5 * 1000;
+            println!("Send value: {:?}", value);
+            let status = esp_now
+                .send(&BROADCAST_ADDRESS, &[value])
+                .unwrap()
+                .wait();
+            println!("Send broadcast status: {:?}", status)
+        }
+
+        VALUE = value;
+
+        NEXT_SEND_TIME = Some(next_send_time);
+        ESP_NOW = Some(esp_now);
+    }
+}
+
+/// Stop condition function for task to execute.
+fn stop_condition_fn() -> bool {
+    return false;
+}
+
+#[entry]
+fn main() -> ! {
+    // Initialize Martos.
+    init_system();
+    // Add task to execute.
+    TaskManager::add_task(setup_fn, loop_fn, stop_condition_fn);
+    // Start task manager.
+    TaskManager::start_task_manager();
+}
